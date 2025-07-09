530a87d52121680183e6762d146b726b5ac8aad1
Trevor6545 <novaktrevor7@gmail.com>
2025-07-08 23:26:03 -0700

    started working on firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzZzibSdACeyDexBmo727JAEci0vUDpyQ",
+  authDomain: "mentat-d8366.firebaseapp.com",
+  projectId: "mentat-d8366",
+  storageBucket: "mentat-d8366.firebasestorage.app",
+  messagingSenderId: "90552056477",
+  appId: "1:90552056477:web:d292f4961a8e6ce2857274",
+  measurementId: "G-GYQEDW5T3T"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
